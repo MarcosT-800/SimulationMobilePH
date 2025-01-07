2dea28823f993425a33a5d024fba889d1165e95d
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2025-01-07 20:51:27 +0000

    feat: Styled GetStarted + Modal Login 1

--- mobile/src/pages/GetStarted.tsx
+++ mobile/src/pages/GetStarted.tsx
@@ -7,15 +7,17 @@ import {
     IonToolbar,
     IonTitle,
     IonPage,
+    IonInput
 } from "@ionic/react";
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 
 import "./main.css";
 
 function GetStarted() {
+    const modal = useRef<HTMLIonModalElement>(null);
 
     return (
-        <div className="block">
+        <div className="body">
             <div className="card">
                 <img alt="Silhouette of mountains" src="/GetStartedLogo.png" />
             </div>
@@ -23,10 +25,28 @@ function GetStarted() {
             <h1 className="title">Bem-vindo ao PH Negócios App</h1>
             <p className="subtitle">Antecipe seu FGTS hoje mesmo de forma rápida e simples com a PH Negócios.</p>
 
-            <button className="button">
-                Iniciar
-            </button>
+
+
             
+        <IonButton id="open-modal" expand="block" color="danger">
+          Open Sheet Modal
+        </IonButton>
+        <IonModal ref={modal} trigger="open-modal" initialBreakpoint={1} breakpoints={[0, 1]}>
+          <div className="block">
+            <div>
+
+          <h1 className="">Bem-vindo ao PH Negócios App</h1>
+          <p className="">Antecipe seu FGTS hoje mesmo de forma rápida e simples com a PH Negócios.</p>
+            </div>
+          <div className="card">
+                <img alt="Silhouette of mountains" src="/PHlogo.png" />
+            </div>
+            <IonInput label="Outline input" labelPlacement="floating" fill="outline" placeholder="Enter text"></IonInput>
+
+            <IonInput label="Outline input" labelPlacement="floating" fill="outline" placeholder="Enter text"></IonInput>
+
+          </div>
+        </IonModal>
         </div>
     );
 }
